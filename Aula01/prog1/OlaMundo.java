444f9f2f422e1ede8eec045d54ece9889759f911
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-02-25 11:07:56 -0300

    Added things

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,14 @@
+class OlaMundo {
+    public static void main(String[] args) {
+        /*
+        Comentário de diversas linhas
+        */
+        System.out.println("Olá Mundo!"); //sysout
+        System.out.println("Salve");
+    }
+}
+
+/*
+Modo de macaco
+    Abrir terminal (Ctrl + Shift + `) -> navegar até a pasta do .java -> Compilar: javac OlaMundo.java -> Rodar: java OlaMundo
+*/
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Linguagens1
- 
+# ecm251-2021-lab2
+Repositório criado para a disciplina Linguagens I.
+
+# Desenvolvedor:
+- Guilherme Samuel de Souza Barbosa
+
+# Linguagens e Frameworks: 
+- Python
+- R
+- Java
+- C
+
+
+
+[comment]: <> (-ls -la; editar readme; git status; git add .; git status; git commit -m "Ajustando README; git push; Logar no github" )
