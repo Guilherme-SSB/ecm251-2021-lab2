af439b476e49f4ae69f0ff5f5728743b749d6fbb
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-03-22 09:53:30 -0300

    Create Atividade1

---
+++ Atividades/Atividade1/src/barbosa/souza/de/samuel/guilherme/Main.java
@@ -0,0 +1,11 @@
+/*
+Guilherme Samuel de Souza Barbosa & 19.00012-0
+*/
+
+package barbosa.souza.de.samuel.guilherme;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
