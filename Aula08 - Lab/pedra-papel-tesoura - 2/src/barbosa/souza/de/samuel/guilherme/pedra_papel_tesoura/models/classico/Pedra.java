724ec68ecd63f57a3e37b4eab7b6be3527c9ebcf
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-04-26 10:52:12 -0300

    Refatoração do código

---
+++ Aula08 - Lab/pedra-papel-tesoura - 2/src/barbosa/souza/de/samuel/guilherme/pedra_papel_tesoura/models/classico/Pedra.java
@@ -0,0 +1,22 @@
+package barbosa.souza.de.samuel.guilherme.pedra_papel_tesoura.models.classico;
+
+import barbosa.souza.de.samuel.guilherme.pedra_papel_tesoura.models.Jogada;
+import barbosa.souza.de.samuel.guilherme.pedra_papel_tesoura.models.bazinga.Lagarto;
+import barbosa.souza.de.samuel.guilherme.pedra_papel_tesoura.models.bazinga.Spoke;
+
+public class Pedra extends Jogada {
+    @Override
+    public Jogada[] jogadasQueGanho() {
+        return new Jogada[] {new Tesoura(), new Lagarto()};
+    }
+
+    @Override
+    public Jogada[] jogadasQuePerco() {
+        return new Jogada[] {new Papel(), new Spoke()};
+    }
+
+    @Override
+    public String toString() {
+        return "Pedra";
+    }
+}
