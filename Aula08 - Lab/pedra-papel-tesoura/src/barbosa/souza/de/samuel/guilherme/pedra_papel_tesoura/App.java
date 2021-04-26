b996e2c96d63e5f81eeb9a61bdca31a0e9f203b9
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-04-26 10:52:19 -0300

    Refatoração do código

---
+++ Aula08 - Lab/pedra-papel-tesoura/src/barbosa/souza/de/samuel/guilherme/pedra_papel_tesoura/App.java
@@ -0,0 +1,10 @@
+package barbosa.souza.de.samuel.guilherme.pedra_papel_tesoura;
+
+import barbosa.souza.de.samuel.guilherme.pedra_papel_tesoura.controladores.Sistema;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Sistema sistema = new Sistema();
+        sistema.run();
+    }
+}
