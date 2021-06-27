553412a7ce60ca7cf206fd12fd0e594969e3136e
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-06-26 21:47:03 -0300

    Adicionado link do projeto

--- Atividades/Atividade2/src/consoleti/gustavo/t2/App.java
+++ Atividades/Atividade2/src/consoleti/gustavo/t2/App.java
@@ -2,6 +2,8 @@
  * @author Guilherme Samuel de Souza Barbosa 19.00012-0
  * @author Gustavo Consoleti Ramirez de Souza 19.00715-9
  * 
+ * Link do projeto no github: https://github.com/Guilherme-SSB/ecm251-2021-lab2/tree/main/Atividades/Atividade2
+ * 
  */
 
 package consoleti.gustavo.t2;
